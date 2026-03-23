ce135d3844d9b04864e928c2e228c9ec4a823ac1
ameenzr <ameennazerpk7@@gmail.com>
2026-03-23 09:04:39 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+function contact() {
+  alert("Add WhatsApp link or form here 🚀");
+}
